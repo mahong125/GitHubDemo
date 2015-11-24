62b1a43ac00ac33e4dee50c58b60a0b0b5ab39ac
mahong125 <@hong_xiazhi@163.com>
2015-11-24 21:52:17 +0800

    first commit

---
+++ GithubDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  GithubDemo
+//
+//  Created by mahong on 15/11/24.
+//  Copyright © 2015年 mahong. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AFNetworking.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"commit to github");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'GithubDemo' do
+pod 'AFNetworking', '~> 3.0.0-beta.1'
+
+end
+
+target 'GithubDemoTests' do
+
+end
+
+target 'GithubDemoUITests' do
+
+end
+
